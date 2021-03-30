9ce76386218337d12638d3d59fd9c66f75d8b99f
unknown <acamalraj@gmail.com>
2021-03-30 22:34:35 +0530

    add program to check two lines are equal

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;  
-  
 public class LineComparisonComputation { 
+	static double temp = 0.0;
+
 	public static void line(double x1,double x2,double y1,double y2) {
 		
 		double result= Math.sqrt( ((x2-x1)*(x2-x1))-((y2-y1)*(y2-y1)) );
 	    System.out.println("length of line = "+result);
 		
+		if(temp==0) {
+			temp=result;
+			System.out.println("enter the values for second line");
+		}
+		else if(temp==result) {
+			System.out.println("lines is of equal length");
+		}
 	}
   
    public static void main(String[] args) {  
-	   
+	   for(int i=0;i<2;i++) {
 	   Scanner x1 = new Scanner(System.in);  
        System.out.print("Enter a number x1: ");  
        int n1 = x1.nextInt(); 
@@ -27,5 +35,9 @@ public class LineComparisonComputation {
        int n4 = y2.nextInt(); 
        
        line(n1,n2,n3,n4);
+	   }
+      
+      
+      
    }
 }  
