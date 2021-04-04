ff6e647d39789d4e4395e60c29644f2418c03dc4
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-04 19:19:28 +0530

    Update LineComparisonComputation.java

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,51 +1,38 @@
-import java.util.Scanner;  
+ 
 public class LineComparisonComputation { 
 	static double temp = 0.0;
 
-	public static void line(double x1,double x2,double y1,double y2) {
+	public static double line(double x1,double x2,double y1,double y2) {
 		
 		double result= Math.sqrt( ((x2-x1)*(x2-x1))-((y2-y1)*(y2-y1)) );
-	    System.out.println("length of line = "+result);
+	    System.out.println("\nlength of line = "+result);
+	    return result;
 		
-		if(temp==0) {
-			temp=result;
-			System.out.println("enter the values for second line");
-			
+	}
+	public static void CompareTo(double result1,double result) {
+
+		if(result1==result) {
+			System.out.println("\nlines is of equal length");
 		}
-		else if(temp==result) {
-			System.out.println("lines is of equal length");
+		else if(result1<result) {
+			System.out.println("\nlength of line1<line2");
 		}
-		else if(temp<result) {
-			System.out.println("length of line1<line2");
-		}
-		else if(temp>result) {
-			System.out.println("length of line1>line2");
+		else if(result1>result) {
+			System.out.println("\nlength of line1>line2");
 		}
 	}
   
    public static void main(String[] args) {  
-	   for(int i=0;i<2;i++) {
-	   Scanner x1 = new Scanner(System.in);  
-       System.out.print("Enter a number x1: ");  
-       int n1 = x1.nextInt(); 
-       
-       Scanner x2 = new Scanner(System.in);  
-       System.out.print("Enter a number x2: ");  
-       int n2 = x2.nextInt(); 
-       
-       Scanner y1 = new Scanner(System.in);  
-       System.out.print("Enter a number y1: ");  
-       int n3 = y1.nextInt(); 
-       
-       Scanner y2 = new Scanner(System.in);  
-       System.out.print("Enter a number y2: ");  
-       int n4 = y2.nextInt(); 
-       
-       line(n1,n2,n3,n4);
+	  LineComparisonComputation L1 = new  LineComparisonComputation();
+	  System.out.println("\nline1 = x1: "+10+",x2: "+5+",y1: "+10+",y2: "+6);
+	 double v1= L1.line(10, 5, 10, 6);
+	  System.out.println("\nline2 = x1: "+10+",x2: "+5+",y1: "+10+",y2: "+7);
+	  double v2 =L1.line(10, 5, 10, 7);
+	  CompareTo(v1,v2);
        
 	   }
       
       
 	  
    }
-}  
+ 
